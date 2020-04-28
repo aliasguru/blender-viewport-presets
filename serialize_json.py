1e89e9adf65ec23126a877577faa471d463f2f0a
aliasguru <message@aliasguru.eu>
2020-04-28 16:02:57 +0200

    feat: add module to serialize the presets

---
+++ serialize_json.py
@@ -0,0 +1,14 @@
+'''
+Created on 28.04.2020
+
+@author: r.trummer
+'''
+import bpy
+import json
+from . import viewport_presets_addonprefs as vp
+
+preset_dir = 'json_presets'
+
+
+def get_presets():
+	pass
